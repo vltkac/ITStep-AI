0b81660258c027c4175fa9a5b70d8503155d7639
vltkac <vladtkachuk135@gmail.com>
2025-11-18 22:51:17 +0100

    update

--- data/lesson2/main.py
+++ data/lesson2/main.py
@@ -1,51 +1,33 @@
+import cv2
+
 # Завдання 1
-# Відкрийте зображення data/lesson2/marbles.png.
-# Використайте кольорову сегментацію для отримання масок до
-# кульок:
-#  синього кольору
+# Відкрийте зображення data/Lenna.png. Прочитайте маски
+# data/mask1.png та data/mask2.png
 
+image_lenna = cv2.imread('Lenna.png')
+mask1 = cv2.imread('mask1.png', cv2.IMREAD_GRAYSCALE)
+mask2 = cv2.imread('mask2.png', cv2.IMREAD_GRAYSCALE)
 
-# import cv2
+# Виведіть ту частину зображення, яка відповідає:
+# mask1
+# mask2
+# mask1 і mask2
 
+result1 = cv2.bitwise_and(image_lenna, image_lenna, mask=mask1)
+result2 = cv2.bitwise_and(image_lenna, image_lenna, mask=mask2)
+cv2.imshow('Result Mask 1', result1)
+cv2.imshow('Result Mask 2', result2)
 
-# marbles_orig = cv2.imread('marbles.png')
-# cv2.imshow('original', marbles_orig)
+combined_mask = cv2.bitwise_or(mask1, mask2)
+result_combined = cv2.bitwise_and(image_lenna, image_lenna, mask=combined_mask)
+cv2.imshow('Result Combined Mask', result_combined)
 
-# marbles_hsv = cv2.cvtColor(marbles_orig, cv2.COLOR_BGR2HSV)
-# blue_mask = cv2.inRange(marbles_hsv, (90, 220, 10), (125, 255, 255))
-# cv2.imshow('blue mask', blue_mask)
-# h: 90-125, s: 220-255, v: 10-255
+cv2.waitKey(0)
 
-# blue_mask = blue_mask.astype(bool)
+# Завдання 2
+# Виведіть зображення. Підберіть самостійно межі
 
-# marbles_orig[blue_mask] = 255
-
-# cv2.imshow('white blue', marbles_orig)
-
-#  зеленого і червоного
-# green_mask = cv2.inRange(marbles_hsv, (50, 200, 10), (70, 255, 255))
-# h: 50-70, s: 200-255, v: 10-255
-# red_orange_mask = cv2.inRange(marbles_hsv, (0, 200, 30), (5, 255, 255))
-# h: 0-5, s: 200-255, v: 20-255
-# red_blue_mask = cv2.inRange(marbles_hsv, (175, 200, 30), (180, 255, 255))
-# h: 175-180, s: 200-255, v: 20-255
-# red_mask = cv2.bitwise_or(red_orange_mask, red_blue_mask)
-# red_green_mask = cv2.bitwise_or(red_mask, green_mask)
-
-# grey_marbles = cv2.cvtColor(marbles_orig, cv2.COLOR_BGR2GRAY)
-# cv2.imshow('grey img', grey_marbles)
-
-#  білого
-# white_mask = cv2.inRange(grey_marbles, 210, 255)
-
-#  чорного
-# black_mask = cv2.inRange(grey_marbles, 0, 15)
-
-# cv2.imshow('red org mask', red_orange_mask)
-# cv2.imshow('red blue mask', red_blue_mask)
-# cv2.imshow('red', red_mask)
-# cv2.imshow('green mask', green_mask)
-# cv2.imshow('red green mask', red_green_mask)
-# cv2.imshow('black mask', black_mask)
-# cv2.imshow('white mask', white_mask)
-# cv2.waitKey(0)
+baboo_img = cv2.imread('baboo.jpg', cv2.IMREAD_GRAYSCALE)
+eyes_img = baboo_img[10:45, 55:210]
+cv2.imshow('Baboo Eyes', eyes_img)
+cv2.waitKey(0)
