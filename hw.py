bdcd427b4d18a8165afd8d07b06e8a593a7d7f58
vltkac <vladtkachuk135@gmail.com>
2025-11-25 21:16:23 +0100

    hw committed

--- hw.py
+++ hw.py
@@ -1,58 +1,41 @@
-# Завдання 2
-# Відкрийте відео з файлу data\lesson7\text.mp4. Проведіть
-# бінарізацію кадрів та збережіть в новий файл.
-
-
 import cv2
 
 
-cap = cv2.VideoCapture('text.mp4')
+cap = cv2.VideoCapture('meter.mp4')
 
 fourcc = cv2.VideoWriter_fourcc(*"mp4v")
-fps = int(cap.get(cv2.CAP_PROP_FPS))
 width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+fps = int(cap.get(cv2.CAP_PROP_FPS))
 
-# cap.set(cv2.CAP_PROP_FRAME_WIDTH, int(width * 0.5))
-# cap.set(cv2.CAP_PROP_FRAME_HEIGHT, int(height * 0.5))
-
-writer = cv2.VideoWriter(
-    'new_text.mp4',  # шлях до файлу
-    fourcc,        # кодек
-    fps,
-    (width // 2, height // 2),
-    isColor=False   # чи кадри кольорові
-)
-
+writer = cv2.VideoWriter('meter_binar.mp4', fourcc, fps, (width // 2, height // 2), isColor=False)
 
 while True:
-    success, img = cap.read()
+    success, frame = cap.read()
 
     if not success:
         break
 
-    img = cv2.resize(img, None, fx=0.5, fy=0.5)
+    if cv2.waitKey(20) & 0xFF == 27:
+        break
 
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    frame = cv2.resize(frame, None, fx=0.5, fy=0.5)
 
-    img = cv2.GaussianBlur(img, (9, 9), 1)
-    img = cv2.bilateralFilter(img, 7, 75, 75)
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    img = cv2.adaptiveThreshold(img,
+    gray_frame = cv2.GaussianBlur(gray_frame, (5, 5), 1)
+    gray_frame = cv2.bilateralFilter(gray_frame, 9, 75, 75)
+
+    gray_frame = cv2.adaptiveThreshold(gray_frame,
                                 255,
                                 cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
                                 cv2.THRESH_BINARY,
                                 11,
-                                4
+                                2
                             )
 
-
-    cv2.imshow('orig', img)
-
-    writer.write(img)
-
-    if cv2.waitKey(20) & 0xFF == ord('q'):
-        break
+    cv2.imshow('meter_binar.mp4', gray_frame)
+    writer.write(gray_frame)
 
 
 cap.release()
